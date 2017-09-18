41be5a2100be9eb96bd1f182515616135cfde94a
Tuterangiwhaitiri Pleydell <tuterangi@gmail.com>
2017-09-18 12:00:44 +1200

    set up basic API calls

---
+++ js/script.js
@@ -0,0 +1,45 @@
+var APIKey = "BjjvUIbXE6c4XfLAYUIyPszNDSzI4CP8"
+projectSearch();
+userSearch();
+
+function projectSearch() {
+	be(APIKey).project.search('graphics', function success(results){
+	var searchResults = results.projects;
+	console.log(searchResults);	
+		for (var i = 0; i < 10; i++) {
+			var ownerList = [];
+			for (var j = 0; j < searchResults[i].owners.length; j++) {
+				ownerList.push(` ${searchResults[i].owners[j].display_name}`);
+			}
+			$('#test1').append(`
+				<div class="projectName">${searchResults[i].name}</div>
+				<div class="projectOwners">${ownerList}</div>
+				<div>${searchResults[i].created_on}</div
+				<div class="projectURL">${searchResults[i].url}</div>
+				<div class="projectCover"><img src="${searchResults[i].covers[404]}"/></div>
+				<div class="projectStats">Likes:${searchResults[i].stats.appreciations} Views:${searchResults[i].stats.views} Comments:${searchResults[i].stats.comments} 
+				<br><br>
+			`);
+		}
+	})
+}
+
+function userSearch() {
+	be(APIKey).user.search('graphics', function success(results){
+	var searchResults = results.users;
+	console.log(searchResults);	
+		for (var i = 0; i < 10; i++) {
+			var ownerList = [];
+			for (var j = 0; j < searchResults[i].fields.length; j++) {
+				ownerList.push(` ${searchResults[i].fields[j]}`);
+			}
+			$('#test2').append(`
+				<div class="userName">${searchResults[i].display_name}</div>
+				<div class="userURL">${searchResults[i].url}</div>
+				<div class="userCover"><img src="${searchResults[i].images[276]}"/></div>
+				<div class="userStats">Likes:${searchResults[i].stats.appreciations} Views:${searchResults[i].stats.views} Comments:${searchResults[i].stats.comments}
+				<br><br>
+			`);
+		}
+	})
+}
